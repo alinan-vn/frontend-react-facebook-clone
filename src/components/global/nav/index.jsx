a4670531e3e87ddc8cb2f22f9bd60b900d4eb48c
Alejandro Linan <jandrolinan@gmail.com>
2022-02-01 12:05:36 -0600

    changes to nav scss and variables scss

--- src/components/global/nav/index.jsx
+++ src/components/global/nav/index.jsx
@@ -7,24 +7,28 @@ class Navigation extends React.Component {
             <div>
                 <div className='nav'>
                     <div className='nav_left'>
-                        <a className='nav_logo'>F</a>
-                        <input type="text" placeholder='Search Facebook' />
+                        <a className='nav_logo nav_left-item'>F</a>
+                        <input 
+                            className='nav_left-item nav_left-search' 
+                            type="text" 
+                            placeholder='Search Facebook'
+                        />
                     </div>
 
                     <div className='nav_middle'>
-                        <a>H</a>   {/* Push, Home */}
-                        <a>C</a>   {/* Connect */}
-                        <a>V</a>   {/* View */}
-                        <a>M</a>   {/* Marketplace */}
-                        <a>G</a>   {/* Gaming */}
+                        <a className='nav_middle-item'>H</a>   {/* Push, Home */}
+                        <a className='nav_middle-item'>C</a>   {/* Connect */}
+                        <a className='nav_middle-item'>V</a>   {/* View */}
+                        <a className='nav_middle-item'>M</a>   {/* Marketplace */}
+                        <a className='nav_middle-item'>G</a>   {/* Gaming */}
                     </div>
 
                     <div className='nav_right'>
-                        <a>Me</a>              {/* Push, Profile */}
-                        <a>Menu</a>
-                        <a>Messenger</a>
-                        <a>Notifications</a>
-                        <a>Account</a>
+                        <a className='nav_right-item'>Me</a>              {/* Push, Profile */}
+                        <a className='nav_right-item'>Menu</a>
+                        <a className='nav_right-item'>Messenger</a>
+                        <a className='nav_right-item'>Notifications</a>
+                        <a className='nav_right-item'>Account</a>
                     </div>
                 </div>
             </div>
