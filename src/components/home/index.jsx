f640766e361f5c12bcec3efe9ec44d787d21d730
Alejandro Linan <jandrolinan@gmail.com>
2022-02-26 15:59:21 -0600

    changed the background of body within public/index.html rather than having it in the scss. easier to set as a default and edit the componenets/divs with scss

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+    faCirclePlus
+        } from '@fortawesome/free-solid-svg-icons'
+
 class Home extends React.Component {
 
     render(){
         return(
-            <div>
-                Homeeee
+            <div className='home-fcus'>
+                <div className='home-fcus_create-story'>
+                    <FontAwesomeIcon className='home-fcus_create-story_icon' icon={faCirclePlus} size="2x" />
+                    <div className='home-fcus_create-story_text'>
+                        <p className='home-fcus_create-story_text-top'>Create story</p>
+                        <p className='home-fcus_create-story_text-bottom'>Share a photo or write something.</p>
+                    </div>    
+                </div>
+                <div>
+                    What's on your mind
+                </div>
+                <div>
+                    Create Room
+                </div>
+                <div>
+                    People you may know
+                </div>
+                <div>
+                    Post Feed
+                </div>
             </div>
         )
     }
