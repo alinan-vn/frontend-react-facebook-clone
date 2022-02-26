66e65aee9b8b458e3472548ab4d718ca6f4097fc
Alejandro Linan <jandrolinan@gmail.com>
2022-02-26 16:40:51 -0600

    input home div scss and jsx

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { 
-    faCirclePlus
+    faCirclePlus,
+    faUser,
+    faVideo,
+    faPhotoFilm,
+    faFaceSmileBeam
         } from '@fortawesome/free-solid-svg-icons'
 
 class Home extends React.Component {
@@ -11,21 +15,70 @@ class Home extends React.Component {
         return(
             <div className='home-fcus'>
                 <div className='home-fcus_create-story'>
-                    <FontAwesomeIcon className='home-fcus_create-story_icon' icon={faCirclePlus} size="2x" />
+                    <FontAwesomeIcon 
+                        className='home-fcus_create-story_icon' 
+                        icon={faCirclePlus} size="2x" 
+                    />
                     <div className='home-fcus_create-story_text'>
-                        <p className='home-fcus_create-story_text-top'>Create story</p>
-                        <p className='home-fcus_create-story_text-bottom'>Share a photo or write something.</p>
+                        <p className='home-fcus_create-story_text-top'>
+                            Create story
+                        </p>
+                        <p className='home-fcus_create-story_text-bottom'>
+                            Share a photo or write something.
+                        </p>
                     </div>    
                 </div>
-                <div>
-                    What's on your mind
+
+                <div className='home-fcus_whats-on-your-mind'>
+                    <div className='home-fcus_whats-on-your-mind_top'>
+                        <FontAwesomeIcon 
+                            className='home-fcus_whats-on-your-mind_top_icon' 
+                            icon={faUser} size="2x" 
+                        />
+                        <input 
+                            className='home-fcus_whats-on-your-mind_top_input'
+                            placeholder="What's on your mind, USER?" 
+                        />
+                    </div>
+
+                    <hr className='home-fcus_whats-on-your-mind_mid-hr' />
+
+                    {/* bootstrap css magic (to divide into three even horizontal containers) divs classes have container-liquid, row, and col */}
+                    <div className='container-liquid home-fcus_whats-on-your-mind_bottom'>
+                        <div className='row'>  
+                            <p className='col home-fcus_whats-on-your-mind_bottom'>
+                                <FontAwesomeIcon 
+                                    className='home-fcus_whats-on-your-mind_bottom_icon' 
+                                    icon={faVideo} size="2x" 
+                                />
+                                <p>Live Video</p>
+                            </p>
+                            <p className='col home-fcus_whats-on-your-mind_bottom'>
+                                <FontAwesomeIcon 
+                                    className='home-fcus_whats-on-your-mind_bottom_icon' 
+                                    icon={faPhotoFilm} size="2x" 
+                                />
+                                <p>Photo/Video</p>
+                            </p>
+                            <p className='col home-fcus_whats-on-your-mind_bottom'>
+                                <FontAwesomeIcon 
+                                    className='home-fcus_whats-on-your-mind_bottom_icon' 
+                                    icon={faFaceSmileBeam} size="2x" 
+                                />
+                                <p>Feeling/activity</p>
+                            </p>
+                        </div>
+                    </div>
                 </div>
+
                 <div>
                     Create Room
                 </div>
+
                 <div>
                     People you may know
                 </div>
+
                 <div>
                     Post Feed
                 </div>
